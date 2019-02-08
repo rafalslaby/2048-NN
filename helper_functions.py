4531f8abc390c46bc7200b0ec536b4a7bc0a49ce
rafalslaby <rafals222@outlook.com>
2019-02-08 11:37:00 +0100

    Fix reward functions

--- helper_functions.py
+++ helper_functions.py
@@ -65,21 +65,21 @@ def make_decreasing_epsilon(constant, min_eps):
 
 
 def count_reward(result: StepResult):
-    return len(result.board_move_result)
+    return len(result.board_move_result.merged_values)
 
 
 count_reward.short_name = 'cr'
 
 
 def normalized_count_reward(result: StepResult):
-    return len(result.board_move_result) / 8
+    return len(result.board_move_result.merged_values) / 8
 
 
 normalized_count_reward.short_name = 'ncr'
 
 
 def dynamic_normalized_count_reward(result: StepResult):
-    return len(result.board_move_result - 1) / 8
+    return (len(result.board_move_result.merged_values) - 1) / 8
 
 
 dynamic_normalized_count_reward.short_name = 'ncr'
@@ -88,7 +88,7 @@ dynamic_normalized_count_reward.short_name = 'ncr'
 def punishing_normalized_count_reward(result: StepResult):
     if result.is_game_over:
         return -1
-    return (len(result.board_move_result) - 1) / 8
+    return len(result.board_move_result.merged_values) / 8
 
 
 punishing_normalized_count_reward.short_name = 'pncr'
@@ -97,7 +97,7 @@ punishing_normalized_count_reward.short_name = 'pncr'
 def dynamic_punishing_normalized_count_reward(result: StepResult):
     if result.is_game_over:
         return -1
-    return (len(result.board_move_result)) / 8
+    return (len(result.board_move_result.merged_values) - 1) / 8
 
 
 count_reward.short_name = 'dpncr'
