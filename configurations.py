85a9690eba2083e269e510ebcd0aa1ec9c88465b
rafalslaby <rafals222@outlook.com>
2019-02-08 19:31:08 +0100

    Add configurations blocks

--- configurations.py
+++ configurations.py
@@ -20,7 +20,7 @@ BATCH_SIZES = [8, 32, 64, 1000]
 DOUBLE_Q_LEARNING_OPTS = [True, False]
 MEMORY_SIZES = [3_000, 10_000, 100_000, 1_000_000]
 LEARN_EACH_OPTS = [4, 10, 30, 100]
-MIN_EPS_OPTS = [0, 0.07, 0.1, 0.15, 0.2]
+MIN_EPS_OPTS = [0, 0.07, 0.1, 0.15]
 REWARD_FUNCS = [count_reward, power_reward, punishing_power_reward, dynamic_punishing_power_reward, value_reward,
                 time_reward_minus, time_reward_plus, dynamic_time_reward_plus, time_reward_plus_minus,
                 dynamic_time_reward_plus_minus]
@@ -40,8 +40,6 @@ UPDATE_TARGETS_EACH_TRAIN_OPTS = [4, 10, 100]
 CONVOLUTIONAL_LAYERS = [[(128, 3), (128, 2)], [(256, 3), (256, 2)], [(64, 3), (64, 2)], [(128, 3), (256, 2)]]
 CONV_ACTIVATIONS = ['linear', 'relu']
 
-MIN_EPS_OPTS = [0.1]
-
 ####
 # Normalized:
 ####
@@ -49,8 +47,52 @@ MIN_EPS_OPTS = [0.1]
 REWARD_FUNCS = [punishing_normalized_count_reward, dynamic_normalized_count_reward, normalized_count_reward,
                 dynamic_punishing_normalized_count_reward]
 STATE_MAP_FUNCTIONS = [norm_16, div_by_max]
-# no need to have many deep layers with convolutions
+
+######
+# Big net
+######
+DEEP_LAYERS_SIZES = [[500, 300, 200, 200, 100]]
+CONVOLUTIONAL_LAYERS = [[]]
+
+#####
+# Convolutions
+#####
 DEEP_LAYERS_SIZES = [[256], [256, 256], [64] * 3, [128], [128 * 2]]
+CONVOLUTIONAL_LAYERS = [[(128, 3), (128, 2)], [(256, 3), (256, 2)], [(64, 3), (64, 2)], [(128, 3), (256, 2)]]
+
+#####
+# Fast testnig
+#####
+SAVE_MODELS_FREQ = 50_000
+LOG_PROGRESS_FREQ = 1_500
+STEPS = 150_000
+ZERO_EPS_STEP = STEPS - 25_000
+MEMORY_STATS_EACH_STEPS = 1_000
+DIAG_EVALUATE_EACH_GAMES = 50
+EPSILON_CONSTATNS = [3_000, 5_000, 10_000]
+
+#####
+# Long training
+#####
+SAVE_MODELS_FREQ = 50_000
+LOG_PROGRESS_FREQ = 1_500
+STEPS = 750_000
+ZERO_EPS_STEP = STEPS - 100_000
+MEMORY_STATS_EACH_STEPS = 50_000
+DIAG_EVALUATE_EACH_GAMES = 1000
+EPSILON_CONSTATNS = [30_00, 50_000, 100_000]
+
+#####
+# Really long training
+#####
+
+SAVE_MODELS_FREQ = 250_000
+LOG_PROGRESS_FREQ = 2_500
+STEPS = 10_000_000
+ZERO_EPS_STEP = STEPS - 500_000
+MEMORY_STATS_EACH_STEPS = 100_000
+DIAG_EVALUATE_EACH_GAMES = 10_000
+EPSILON_CONSTATNS = [30_00, 50_000, 100_000]
 
 TrainingConf = recordclass('TrainingConf',
                            'allow_illegal min_eps optimizer loss conv_activation conv_layers layers_size output_activation batch_size '
